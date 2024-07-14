e7522cb81b2b5b461dcc9b61381ec30aca48f01e
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-07-14 19:14:54 +0530

    initial commit

--- Intermediate/pattern6.cpp
+++ Intermediate/pattern6.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main(){
-    for(int i=1; i<=5; i++){
-        for(int j =1; j<=5; j++){
-            if(i>j){
-                cout<<" ";
-            } else{
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            if(i+j>=5-1){
                 cout<<"*";
+            } else{
+                cout<<" ";
             }
         }
         cout<<endl;
