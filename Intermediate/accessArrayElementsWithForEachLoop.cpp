abbb37db0146b73bb2ab6392dc95f2782f9dc5da
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-07-14 11:18:17 +0530

    initial commit

---
+++ Intermediate/accessArrayElementsWithForEachLoop.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    float A[] = {2.5f, 5.6f, 9, 8, 7};  // size of array == number of elements
+
+    for(int x:A)    // x is of int type but array and its elements is of float type
+        cout<<x<<endl;  //  So, typecast happened and values after decimal truncated.
+    
+    cout<<"====="<<endl;
+
+    for(float x:A)  // x is of float type
+        cout<<x<<endl;
+    
+    cout<<"====="<<endl;
+
+    for(auto x:A)   //  automatically identified the data type of array so of x
+        cout<<x<<endl;
+
+    return 0;
+}
