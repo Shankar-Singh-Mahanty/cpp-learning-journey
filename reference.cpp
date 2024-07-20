b725f688573cd3572a26c758a7b07b4cbe511862
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-07-20 18:14:15 +0530

    initial commit

---
+++ reference.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 10;
+    int &y = x;     // Declaration & Initialisation of reference should be done together.
+    //  now this Y can't be used as nickname for any other variable as it is already used for X.
+    y++;
+    x++;
+    cout<<x<<endl;
+
+    cout<<&x<<" == "<<&y<<endl;
+    cout<<"Both X and Y are the names of same variable";
+
+    return 0;
+}
