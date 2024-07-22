cf530fb641118cd028cbb8131411aafe78cffae1
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-07-22 09:18:49 +0530

    initial commit

--- Intermediate/subStringSearch.cpp
+++ Intermediate/subStringSearch.cpp
@@ -5,14 +5,23 @@ using namespace std;
 int main(){
     char s1[] = "Programming";
     char s2[] = "gram";
-    char s3[] = "m";
-    char s4[] = "kite";
+    char s3[] = "Gram";
+    char s4[] = "m";
+    char s5[] = "kite";
+    
 
     cout<<strstr(s1,s2)<<endl;
-    cout<<strstr(s1,s3)<<endl;
 
-    if(strstr(s1,s4) != NULL){
-        cout<<strstr(s1,s4)<<endl;
+    if(strstr(s1,s3) != NULL){
+        cout<<strstr(s1,s3)<<endl;
+    } else {
+        cout<<"Not Found becoz of case senitivity"<<endl;
+    }
+
+    cout<<strstr(s1,s4)<<endl;
+
+    if(strstr(s1,s5) != NULL){
+        cout<<strstr(s1,s5)<<endl;
     } else {
         cout<<"Not Found"<<endl;
     }
