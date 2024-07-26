c8b2ad7bc5021fa71080495a6c513422123c1fd1
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-07-26 18:03:23 +0530

    initial commit

---
+++ Intermediate/stringClass.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string str;
+    cout<<"Enter a string: ";
+    getline(cin, str);
+
+    cout<<str<<endl;
+
+    // Issue of reading using cin doesn't work here 
+    string str1, str2;
+
+    cout<<"Enter str1: ";
+    cin>>str1;
+    cout<<str1<<endl;
+
+    cout<<"Enter str2: "<<endl;
+    cin>>str2;
+    cout<<str2<<endl;
+
+
+    return 0;
+}
