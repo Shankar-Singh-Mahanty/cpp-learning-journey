cc11306c3ad45e2df461dc0a10ae015ca737c290
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-08-03 12:16:05 +0530

    initial commit

--- Intermediate/scopingRule.cpp
+++ Intermediate/scopingRule.cpp
@@ -12,7 +12,7 @@ int main(){
     cout<<x<<endl;  // local scope
     // we can't able to access the global variable inside main function
     // To access : -
-    cout<<::x<<endl;
-    
+    cout<<::x<<endl;    // global scope
+
     return 0;
 }
