43a8835b9992e5a2f3899334473f915609ecece9
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-08-03 12:15:27 +0530

    initial commit

---
+++ Intermediate/scopingRule.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int x = 10;
+
+int main(){
+    int x = 20;
+    {
+        int x  = 30;
+        cout<<x<<endl; // block scpoe
+    }
+    cout<<x<<endl;  // local scope
+    // we can't able to access the global variable inside main function
+    // To access : -
+    cout<<::x<<endl;
+    
+    return 0;
+}
