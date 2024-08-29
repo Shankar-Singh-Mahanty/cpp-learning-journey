18747487847222de26314f5c5f4c810639a7c19c
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-08-29 17:11:40 +0530

    initial commit

---
+++ Advanced/operatorOverloadingFriendFunction.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Complex {
+    private:
+    int real;
+    int img;
+    public:
+    Complex(int r=0,int i=0)
+    {
+        real=r;
+        img=i;
+    }
+    void display()
+    {
+        cout<<real<<"+i"<<img<<endl;
+    }
+    friend Complex operator+(Complex c1, Complex c2);
+};
+
+int main(){
+    Complex c1(3,5), c2(7,9), c3;
+
+    c3 = c1 + c2;
+    c3.display();
+
+    return 0;
+}
+
+Complex operator+(Complex c1, Complex c2){
+    Complex t;
+    t.real = c1.real + c2.real;
+    t.img = c1.img + c2.img;
+    return t;
+}
+
+/*
+The friend function is an independent function defiened outside the class
+that is not a member of the class but a friend of the class
+so we didn't use the scope resolution.
+its a global function as well.
+*/
